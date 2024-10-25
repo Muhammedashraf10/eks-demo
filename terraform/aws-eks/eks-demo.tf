468500082ad3cc1cd4327711b46e7c5964aa27a0
Muhammed Ashraf <muhammedashraf192@gmail.com>
2024-10-25 11:06:36 +0300

    Create eks-demo.tf

---
+++ terraform/aws-eks/eks-demo.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_role" "eks-demo" {
+  name = "eks-cluster-demo"
+
+  assume_role_policy = <<POLICY
+  {
+    "Version": "2012-10-17"
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Princpal: {
+           "Service": "eks.amazonaws.com
+        },
+        "Action": "sts.AssumeRole"
+      }
+    ]
+  }
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-AWSEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam:aws:policy/AmazonEKSClusterPolicy"
+  role = aws_iam_role.eks-demo.name
+}
+
+resource "aws_eks_cluster" "eks-demo" {
+ name = "demo-cluster"
+ role_arn = aws_iam_role.eks-demo
+
+ vpc_config {
+   subnet_ids = [
+    aws_subnet.eks-public-subnet-1a,
+    aws_subnet.eks-public-subnet-1b,
+    aws_subnet.eks-pv-subnet-1a,
+    aws_subnet.eks-pv-subnet-1b
+   ]
+ }
+  depends_on = [ aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy ]
+}
