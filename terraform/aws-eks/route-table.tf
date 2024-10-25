d870423bc1e04edb996ad28f570a580fa5b6a95b
Muhammed Ashraf <muhammedashraf192@gmail.com>
2024-10-25 11:46:49 +0300

    Update route-table.tf

--- terraform/aws-eks/route-table.tf
+++ terraform/aws-eks/route-table.tf
@@ -46,7 +46,7 @@ resource "aws_route_table_association" "public-us-esat-1a" {
   route_table_id = aws_route_table.public
 }
 
-resource "aws_route_table_association" "private-us-esat-1b" {
+resource "aws_route_table_association" "public-us-esat-1b" {
   subnet_id = aws_subnet.eks-public-subnet-1b
   route_table_id = aws_route_table.public.id
 }
