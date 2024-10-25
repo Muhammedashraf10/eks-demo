31870d9ff7aca46da183365883779cfe1fa35e36
Muhammed Ashraf <muhammedashraf192@gmail.com>
2024-10-25 12:17:23 +0300

    Update nat-eks.tf

--- terraform/aws-eks/nat-eks.tf
+++ terraform/aws-eks/nat-eks.tf
@@ -7,10 +7,12 @@ resource "aws_eip" "eks-demo-gateway-eip" {
 
 resource "aws_nat_gateway" "eks-demo-nat-gateway" {
   allocation_id = aws_eip.eks-demo-gateway-eip.id
-  subnet_id = aws_subnet.eks-public-subnet-1a
+  subnet_id = aws_subnet.eks-public-subnet-1a.id
 
 
   tags = {
     "Name" = "Nat-GW-eks-demo" 
   }
+
+  depends_on = [ aws_internet_gateway.eks-vpc ]
 }
